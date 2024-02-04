c139e32d5eb25ba1a08372963600dd5243e0bbca
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-04 19:49:56 +0530

    removed htmpdf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import html2pdf from 'html2pdf.js';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['html2pdf.js'], 
+    },
+  },
+});
+
